a1fa4b2b9dd36f040a1a115021cb58e6523759ef
Murugan <smurugan29052001@gmail.com>
2026-05-06 20:15:11 +0530

    Initial commit

---
+++ Chapter01_javascript.js/Basics_01.js
@@ -0,0 +1,12 @@
+// Output to the console
+console.log("Hello, World!");
+
+
+// Variable declaration and initialization
+let a = 10;
+console.log(a);
+
+//process information
+console.log(process.platform);
+console.log(process.arch);
+console.log(process.version);
